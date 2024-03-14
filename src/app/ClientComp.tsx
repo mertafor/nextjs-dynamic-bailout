d10bb0f9ea28b4efe48603caf10f6e0ab42b546d
Mert Daglaroglu <mertdaglaroglu@gmail.com>
2024-03-14 10:19:14 +0300

    feat: SSR and NON SSR components added

--- src/app/ClientComp.tsx
+++ src/app/ClientComp.tsx
@@ -2,15 +2,24 @@
 import dynamic from 'next/dynamic';
 import React from 'react';
 
-const LazyComp = dynamic(
+const LazyCompSSR = dynamic(
   () => import('./LazyImportComponent').then((mod) => mod.LazyImportComponent),
+  { ssr: true }
+);
+
+const LazyCompNonSSR = dynamic(
+  () =>
+    import('./LazyImportComponentSSR').then(
+      (mod) => mod.LazyImportComponentSSR
+    ),
   { ssr: false }
 );
 
 export const ClientComp = () => {
   return (
     <div>
-      <LazyComp />
+      <LazyCompSSR />
+      <LazyCompNonSSR />
     </div>
   );
 };
