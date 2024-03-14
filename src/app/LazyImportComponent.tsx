4d46de0374f82c5d415994d64f8363f280f1fff5
Mert Daglaroglu <mertdaglaroglu@gmail.com>
2024-03-14 10:19:58 +0300

    feat: SSR and NON SSR components added

--- src/app/LazyImportComponent.tsx
+++ src/app/LazyImportComponent.tsx
@@ -3,5 +3,5 @@
 import React from 'react';
 
 export const LazyImportComponent = () => {
-  return <div>Lazy loaded client component</div>;
+  return <div>Lazy loaded component SSR:False</div>;
 };
--- src/app/LazyImportComponentSSR.tsx
+++ src/app/LazyImportComponentSSR.tsx
@@ -3,5 +3,5 @@
 import React from 'react';
 
 export const LazyImportComponentSSR = () => {
-  return <div>Lazy loaded client component</div>;
+  return <div>Lazy loaded component SSR:true</div>;
 };
